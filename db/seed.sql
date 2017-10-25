0d18ef5cea674a93319b2b4b875181e24e3679ac
lcatalano <larissa.catalano@wascholar.org>
2017-10-25 11:46:32 -0400

    db build

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS bars;
+DROP TABLE IF EXISTS events_users;
 
 CREATE TABLE users (
   id BIGSERIAL PRIMARY KEY,
@@ -7,3 +10,23 @@ CREATE TABLE users (
   password_digest VARCHAR NOT NULL,
   token VARCHAR NOT NULL
 );
+
+CREATE TABLE events {
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  time TIMESTAMP NOT NULL
+}
+
+CREATE TABLE bars (
+  id SERIAL PRIMARY KEY,
+  eventId INTEGER,
+  lat DOUBLE PRECISION,
+  long DOUBLE PRECISION,
+  name VARCHAR(255)
+)
+
+CREATE TABLE events_users (
+  eventId INTEGER,
+  userId INTEGER
+)
