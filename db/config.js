1ef380b6bd05e16eeb7780bdb87162cbe9019d1d
Kin L Lim <Lim.Kin@gmail.com>
2017-10-28 17:09:53 -0400

    changes

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')({}),
 
-config = process.env.DATABASE_URL || 'postgres://annarpack@localhost:5432/bar_crawl_db',
+config = process.env.DATABASE_URL || 'postgres://kinllim@localhost:5432/bar_crawl_db',
 db = pgp(config);
 
 module.exports = db;
