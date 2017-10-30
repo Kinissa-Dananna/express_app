4ce221252904f51cae6633f75e8aa6d9d43f3784
Daniel Risdon <drisdon07@gmail.com>
2017-10-30 11:21:34 -0400

    add user image to autocomplete

--- controllers/user_search_controller.js
+++ controllers/user_search_controller.js
@@ -7,9 +7,7 @@ router.get('/:userSearch',
   Auth.restrict,
   User.findByName,
   (req, res) => {
-      const userName = res.locals.userName;
-      const userId = res.locals.userId;
-	    res.json({userName: userName, userId: userId});
+	    res.json(res.locals.users);
   });
 
 
